47b42dcaeb762cadd27353d36366692d1eb52977
NatalyMit <mitenkova91@icloud.com>
2024-02-09 19:08:51 +0100

    add code

--- src/js/exercises.js
+++ src/js/exercises.js
@@ -37,16 +37,17 @@ function createExerciseCard({
   return `<li class = "list-exercises" data-filter="${name}"><div class="options">
   <div class="box-up">
   <div class="box-left">
-  <div class="work-div"><p class="options-item work-div"> WORKOUT</p></div>
-  <div class="rating-stars"><span class="options-item-span">${rating}</span><svg class="icon-star" width="18" height="18"><use href="../svg/icons.svg#icon-star"></use></svg></div></div>
+  <div class="work-div"><p class="options-item work-div">WORKOUT</p></div>
+  <div class="rating-stars"><p class="rating-par">${rating}</p><svg class="icon-star" width="18" height="18"><use href="../svg/icons.svg#icon-star"></use></svg></div></div>
 
   
-  <button type = "button" class="btn-start-arrow">START<svg class="icon-star" width="14" height="14"><use href="../svg/icons.svg#icon-arrow"></use></svg></button>
+  <button type = "button" class="btn-start-arrow">START<svg class="icon-arrow" width="14" height="14"><use href="../svg/icons.svg#icon-arrow"></use></svg></button>
   </div>
            
-            <p class="exercises-par"> <span class="options-item-span"><svg class="icon-star" width="18" height="18"><use href="../svg/icons.svg#icon-running-man"></use></svg>
-            
-            </span>${name}</p>
+            <div class="exercises-par"> 
+            <div class="options-item-span"><svg class="icon-men" width="18" height="18"><use href="../svg/icons.svg#icon-running-man"></use></svg></div>
+            <p class="ex-name">${name}</p>
+            </div>
            
             <p class="options-item"><span class="hid-txt">Burned calories:</span>${burnedCalories}/${time}</p>
             <div class="info-ex">
