e34af8f0fe37c4d34dd5525fcfed11cce548ec78
NatalyMit <142353077+NatalyMit@users.noreply.github.com>
2024-02-09 18:09:09 +0000

    Deploying to gh-pages from @ NatalyMit/test-project@47b42dcaeb762cadd27353d36366692d1eb52977 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,4 +1,4 @@
-(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))n(t);new MutationObserver(t=>{for(const i of t)if(i.type==="childList")for(const c of i.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&n(c)}).observe(document,{childList:!0,subtree:!0});function r(t){const i={};return t.integrity&&(i.integrity=t.integrity),t.referrerpolicy&&(i.referrerPolicy=t.referrerpolicy),t.crossorigin==="use-credentials"?i.credentials="include":t.crossorigin==="anonymous"?i.credentials="omit":i.credentials="same-origin",i}function n(t){if(t.ep)return;t.ep=!0;const i=r(t);fetch(t.href,i)}})();const a=document.querySelector(".exercises-nav-list"),g=document.querySelector(".exercises-container"),h="https://energyflow.b.goit.study/api";a.addEventListener("click",v);l("Muscles").then(d);async function v(s){const{filter:e}=s.target.dataset;if(!e)return;await l(e).then(d);const r=a.querySelector(".active"),n=s.target;r.disabled=!1,n.disabled=!0,r.classList.remove("active"),n.classList.add("active")}async function l(s){return fetch(`${h}/filters?filter=${s}&page=1&limit=12`).then(e=>e.json()).then(e=>e.results)}function m({name:s,filter:e,imgUrl:r}){return`
+(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))n(t);new MutationObserver(t=>{for(const i of t)if(i.type==="childList")for(const c of i.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&n(c)}).observe(document,{childList:!0,subtree:!0});function r(t){const i={};return t.integrity&&(i.integrity=t.integrity),t.referrerpolicy&&(i.referrerPolicy=t.referrerpolicy),t.crossorigin==="use-credentials"?i.credentials="include":t.crossorigin==="anonymous"?i.credentials="omit":i.credentials="same-origin",i}function n(t){if(t.ep)return;t.ep=!0;const i=r(t);fetch(t.href,i)}})();const a=document.querySelector(".exercises-nav-list"),g=document.querySelector(".exercises-container"),v="https://energyflow.b.goit.study/api";a.addEventListener("click",h);l("Muscles").then(d);async function h(s){const{filter:e}=s.target.dataset;if(!e)return;await l(e).then(d);const r=a.querySelector(".active"),n=s.target;r.disabled=!1,n.disabled=!0,r.classList.remove("active"),n.classList.add("active")}async function l(s){return fetch(`${v}/filters?filter=${s}&page=1&limit=12`).then(e=>e.json()).then(e=>e.results)}function m({name:s,filter:e,imgUrl:r}){return`
       <li class="exercise-card" data-filter="${s}">
       <div clas="exercise-card-img">
       <img class="exercises-card-img" src="${r}" alt="${s}">
@@ -11,16 +11,17 @@
     `}function d(s){g.innerHTML=s.map(m)}document.querySelector(".exercises-nav-list");const o=document.querySelector(".exercises-container");document.querySelector(".exercise-card");const y=document.querySelector(".exercises-gallery"),x="https://energyflow.b.goit.study/api/",u="is-hidden";o.addEventListener("click",$);o.classList.remove(u);p("Muscles").then(f);async function $(s){console.log(s.target);const{filter:e}=s.target.dataset;e&&(await p(e).then(f),o.classList.add(u))}async function p(s){return fetch(`${x}exercises?filter=${s}&page=1&limit=12`).then(e=>e.json()).then(e=>e.results)}function L({bodyPart:s,target:e,name:r,burnedCalories:n,rating:t,time:i}){return`<li class = "list-exercises" data-filter="${r}"><div class="options">
   <div class="box-up">
   <div class="box-left">
-  <div class="work-div"><p class="options-item work-div"> WORKOUT</p></div>
-  <div class="rating-stars"><span class="options-item-span">${t}</span><svg class="icon-star" width="18" height="18"><use href="../svg/icons.svg#icon-star"></use></svg></div></div>
+  <div class="work-div"><p class="options-item work-div">WORKOUT</p></div>
+  <div class="rating-stars"><p class="rating-par">${t}</p><svg class="icon-star" width="18" height="18"><use href="../svg/icons.svg#icon-star"></use></svg></div></div>
 
   
-  <button type = "button" class="btn-start-arrow">START<svg class="icon-star" width="14" height="14"><use href="../svg/icons.svg#icon-arrow"></use></svg></button>
+  <button type = "button" class="btn-start-arrow">START<svg class="icon-arrow" width="14" height="14"><use href="../svg/icons.svg#icon-arrow"></use></svg></button>
   </div>
            
-            <p class="exercises-par"> <span class="options-item-span"><svg class="icon-star" width="18" height="18"><use href="../svg/icons.svg#icon-running-man"></use></svg>
-            
-            </span>${r}</p>
+            <div class="exercises-par"> 
+            <div class="options-item-span"><svg class="icon-men" width="18" height="18"><use href="../svg/icons.svg#icon-running-man"></use></svg></div>
+            <p class="ex-name">${r}</p>
+            </div>
            
             <p class="options-item"><span class="hid-txt">Burned calories:</span>${n}/${i}</p>
             <div class="info-ex">
